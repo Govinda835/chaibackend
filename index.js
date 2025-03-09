8399a8765e30bed9872b128f28d9e73e765a08ee
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2025-03-09 10:11:22 +0545

    fist commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+//const port = 3000
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "HELLO WORLD..."
+    })
+})
+
+app.get("/youtube",(req,res)=>{
+    res.status(200).json({
+        message:"chai aur code...."
+    })
+})
+
+app.listen(process.env.port,()=>{
+    console.log(`Example app listening on port ${process.env.port}` );
+})
